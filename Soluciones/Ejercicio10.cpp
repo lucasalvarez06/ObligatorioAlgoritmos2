67972bcfd3f37ebe55a07b28132d26ee1c93f15c
Lucas Alvarez <lucasalvarez2612@gmail.com>
2022-11-24 17:58:05 -0300

    en error

---
+++ Soluciones/Ejercicio10.cpp
@@ -0,0 +1,95 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct ArrayNumeros{
+    public:
+    int *array;
+    int largo;
+
+    ArrayNumeros(int cant){
+        array = new int[largo];
+        largo = cant;
+    }
+
+    ~ArrayNumeros(){
+        delete []array;
+    }
+
+    void QuickSort(int min, int max){
+        int pivot = pivot = array[ (min + max) /2 ]; 
+        int auxMin = min;
+        int auxMax = max;
+        while (auxMin <= auxMax){
+            while( (array[auxMin] < pivot) && (auxMin <= max) )
+            { 
+                auxMin++;
+            } 
+    
+            while( (array[auxMax] > pivot) && (auxMax >= min) )
+            { 
+                auxMax--;
+            } 
+    
+            if(auxMin <= auxMax )
+            { 
+                swap(auxMin, auxMax); 
+                auxMin++;  auxMax--; 
+            }   
+        }
+    
+        if( min < auxMax ) QuickSort(min, auxMax); 
+        if( auxMin < max ) QuickSort( auxMin, max ); 
+
+    }
+
+    void swap(int pos1, int pos2){
+        int aux = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos1] = aux;
+    }
+
+    int ObtenerMaxInt(){
+        int retorno = 0;
+        for (int i = 0; i < largo; i++){
+            retorno += array[i] * Potencia(10, i);
+        }
+    }
+
+    int Potencia(int numero, int exponente){
+        if (exponente == 0) return numero;
+        else return (numero * Potencia(numero, exponente - 1));
+    }
+
+    private:
+};
+
+int main(){
+    //// IMPORTANTE! BORRAR O COMENTAR LAS SIGUIENTES LINEAS  EN TODOS LOS EJERCICIOS DEL OBLIGATORIO. NO PUEDEN ESTAR EN NINGUNA ENTREGA!
+    ifstream myFile("Pruebas/Ejercicio6/3.in.txt");
+    cin.rdbuf(myFile.rdbuf());
+    //Si desean tirar la salida a un archivo, usen las siguientes líneas (si no, sáquenlas):
+    ofstream myFile2("Salidas/Ejercicio10.out.txt");
+    cout.rdbuf(myFile2.rdbuf());
+
+    int largo;
+    int elemento;
+    cin >> largo;
+    ArrayNumeros arrayDado = ArrayNumeros(largo);
+    for (int i = 0; i < largo; i++){
+        cin >> elemento;
+        arrayDado.array[i] = elemento;
+    }
+
+    arrayDado.QuickSort(0,largo - 1);
+
+    int resultado = arrayDado.ObtenerMaxInt();
+    cout << resultado << endl;
+
+    arrayDado.~ArrayNumeros();
+
+
+    return 0;
+}
--- Soluciones/Ejercicio4.cpp
+++ Soluciones/Ejercicio4.cpp
@@ -3,56 +3,147 @@
 #include <fstream>
 #include <string>
 
+#define INF 9999
+using namespace std;
 
-struct MatrizGrafo {
-    private:
-        int **matriz;
-        int largo;
+class Grafo{
+
     public:
-        MatrizGrafo(int cant){
-            largo = cant;
-            matriz = new int[cant][cant];
-            for (int i = 0; i < cant;i++){
-                for(int j = 0; j < cant; j++){
-                    matriz[i][j] = 0;
+        Grafo(int tope){
+            cantElementos = 0;
+            largo = tope;           
+            vertices = new int[tope];
+            matAdy = new int*[tope];
+            for (int i = 0; i < tope; i++){
+                matAdy[i] = new int[tope];
+                for (int j = 0; j < tope; j++)
+                    matAdy[i][j] = 0;
+            } 
+            ExistenVertices = new bool[tope]; 
+            for (int i = 0; i < tope; i++) ExistenVertices[i] = false;         
+        }
+
+        ~Grafo(){
+            for (int i = 0; i < largo; i++){
+                delete[] matAdy[i];
+            }
+            delete []matAdy;
+            delete []ExistenVertices;
+            delete []vertices;
+        }
+
+        void insertarVertice(int v){
+            int i = 0;
+            for(i; i < largo && ExistenVertices[i]; i++)
+            vertices[i] = v;
+            ExistenVertices[i] = true;
+            cantElementos++;
+        }
+
+        void insertarArista(int o, int d){
+            //validar que exista origen y destino
+            int posO = EncontrarVertice(o);
+            int posD = EncontrarVertice(d);
+            if(posO == -1) insertarVertice(o);
+            if(posO == -1) insertarVertice(d);
+            matAdy[posO][posD] = 1;          
+        }
+
+        bool EsTriconexo(){
+            bool retorno = true;
+            for (int i = 0; i < largo && retorno; i++){
+                for (int j = 0; j < largo && retorno; j++){
+                    if (i != j){
+                        bool* visitados = InicializarVisitados(i,j);
+                        int z = 0;
+                        for (z; z != j && z != i; z++);
+                        if (!EsConexoDFS(z, visitados)) retorno = false;
+                        delete []visitados;
+                    }
                 }
             }
+            return retorno;
         }
+        /*void Imprimir(){
+            for (int i = 0; i < largo; i++){
+                cout << vertices[i] << endl;
+            }
+            for (int i = 0; i < largo; i++){
+                for (int j = 0; j < largo; j++)
+                    cout << matAdy[i][j] << endl;
+            }    
+        }*/
 
-        void InsertarArista(int a1, int a2){
-            matriz[a1 - 1][a2 - 1] = 1;
-            matriz[a2 - 1][a1 - 1] = 1;
+    private:
+
+        int* vertices;
+        bool* ExistenVertices;
+        int largo;
+        int cantElementos;
+        int** matAdy;
+
+        bool EsConexoDFS(int pos, bool*& visitados){
+            dfs(pos,visitados);
+            for (int i = 0; i < largo; i++){
+                if (visitados[i] == false) return false;
+            }
+            return true;    
         }
-
-        void EliminarArista(int a1, int a2){
-            matriz[a1 - 1][a2 - 1] = 1;
-            matriz[a2 - 1][a1 - 1] = 1;
-        }
-
         
+        bool* InicializarVisitados(int pos1, int pos2){
+            bool* retorno = new bool[largo];
+            for (int i = 0; i < largo; i++){
+                if (i == pos1 || i == pos2) retorno[i] = true;
+                else retorno[i] = false;
+            }
+            return retorno;
+        }
 
-}
+        void dfs(int o,bool*& vis){
+            vis[o] = true;
+            for (int i = 0; i < largo; i++)
+                if(!vis[i] && matAdy[o][i] == 1)
+                    dfs(i,vis);
+        }
+
+        int EncontrarVertice(int v){
+            for (int i = 0; i < largo; i++)
+            {
+                if(vertices[i] == v && ExistenVertices[i]){
+                    return i;
+                }
+            }
+            return -1;
+        }
+};
 
 int main(){
 
   //// IMPORTANTE! BORRAR O COMENTAR LAS SIGUIENTES LINEAS  EN TODOS LOS EJERCICIOS DEL OBLIGATORIO. NO PUEDEN ESTAR EN NINGUNA ENTREGA!
-  //ifstream myFile("Pruebas/Ejercicio2/100000.in.txt");
-  //cin.rdbuf(myFile.rdbuf());
+  ifstream myFile("Pruebas/Ejercicio4/10.in.txt");
+  cin.rdbuf(myFile.rdbuf());
   //Si desean tirar la salida a un archivo, usen las siguientes líneas (si no, sáquenlas):
-  //ofstream myFile2("Salidas/Ejercicio2.out.txt");
-  //cout.rdbuf(myFile2.rdbuf());
+  ofstream myFile2("Salidas/Ejercicio4.out.txt");
+  cout.rdbuf(myFile2.rdbuf());
 
     int largo;
     cin >> largo;
-    MatrizGrafo matriz = MatrizGrafo(largo);
-
+    Grafo grafo = Grafo(largo);
     int cantAristas;
-    int a1,a2;
     cin >> cantAristas;
+    int a1,a2;
     for (int i = 0; i < cantAristas; i++){
         cin >> a1;
         cin >> a2;
-        matriz.InsertarArista(a1,a2);
+        grafo.insertarArista(a1,a2);
+    }
+
+    //grafo.Imprimir();
+
+    if(grafo.EsTriconexo()){
+        cout << 1 << endl;
+    }else {
+        cout << 0 << endl;
     }
 
 }
